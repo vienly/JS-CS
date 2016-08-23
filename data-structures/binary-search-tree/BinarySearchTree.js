072dae4e637b242215eacd29cf3272737d58f8cb
Gin <vienlyt@gmail.com>
2016-08-23 15:36:37 -0700

    while -> recursion

--- data-structures/binary-search-tree/BinarySearchTree.js
+++ data-structures/binary-search-tree/BinarySearchTree.js
@@ -8,50 +8,96 @@ function BinarySearchTree() {
 
 BinarySearchTree.prototype = {
   insert: function(data) {
-    const node = new Node(data);
+    function insertHelper(data, node) {
+      if (!node) {
+        node = new Node(data);
+        return node;
+      }
 
-    if (this._root === null) {
-      this._root = node;
-    } else {
-      let current = this._root;
+      if (data < node._data) {
+        node._left = insertHelper(data, node._left);
+      } else if (data > node._data) {
+        node._right = insertHelper(data, node._right);
+      }
 
-      while (1) {
-        if (data < current._data) {
-          if (!current._left) {
-            current._left = node;
-            return;
-          } else {
-            current = current._left; // traverse down left side
-          }
-        } else if (data > current._data) {
-          if (!current._right) {
-            current._right = node;
-            return;
-          } else {
-            current = current._right; // traverse down right side
-          }
-        } else {
-          return; // ignore duplicates
-        }
+      return node;
+    }
+
+    this._root = insertHelper(data, this._root);
+  },
+
+
+  find: function(data) {
+    function findHelper(data, node) {
+      if (node._data === data || !node) {
+        return node;
+      } else if (data > node._data) {
+        return findHelper(data, node._right);
+      } else if (data < node._data) {
+        return findHelper(data, node._left);
       }
     }
+
+    return findHelper(data, this._root);
   },
 
-  find: function(data, searchNode) {
-    if (searchNode._data === data || !searchNode) {
-      return searchNode;
-    } else if (data > searchNode._data) {
-      return this.find(data, searchNode._right);
-    } else if (data < searchNode._data) {
-      return this.find(data, searchNode._left);
+
+  inorder: function() {
+    function inorderHelper(node) {
+      let str = '';
+      if (node !== null) {
+        str += inorderHelper(node._left);
+        str += `${node._data}, `;
+        str += inorderHelper(node._right);
+      }
+      return str;
     }
+
+    return inorderHelper(this._root);
   },
+
+
+  preorder: function() {
+    function preorderHelper(node) {
+      let str = '';
+      if (node !== null) {
+        str += `${node._data}, `;
+        str += preorderHelper(node._left);
+        str += preorderHelper(node._right);
+      }
+      return str;
+    }
+
+    return preorderHelper(this._root);
+  },
+
+
+  postorder: function() {
+    function postorderHelper(node) {
+      let str = '';
+      if (node !== null) {
+        str += postorderHelper(node._left);
+        str += postorderHelper(node._right);
+        str += `${node._data}, `;
+      }
+      return str;
+    }
+
+    return postorderHelper(this._root);
+  },
+
 
   delete: function(data) {
-    const node = this.find(data, this._root);
-    if (node) {
-      
-    }
+
+    //   const childCount = (node._left !== null ? 1 : 0) + (node._right !== null ? 1 : 0);
+
+    // const node = this.find(data, this._root);
+    // if (node) {
+    //
+    //   if (node === this._root) {
+    //
+    //   }
+    // }
   },
 };
 
--- data-structures/binary-search-tree/index.js
+++ data-structures/binary-search-tree/index.js
@@ -7,7 +7,13 @@ var bst = new BinarySearchTree();
 bst.insert(5);
 bst.insert(2);
 bst.insert(3);
+bst.insert(1);
+bst.insert(7);
+bst.insert(18);
+bst.insert(9);
 
-console.log(bst);
+console.log(bst.find(1));
 
-console.log(bst.find(2, bst._root));
+console.log('inorder: ', bst.inorder());
+console.log('preorder: ', bst.preorder());
+console.log('postorder: ', bst.postorder());
